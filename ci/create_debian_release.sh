36b2f996144022f15287dd414c6e79c754f716c6
Björn Kirchner <bk@emlix.com>
2025-04-10 13:02:16 +0200

    feat: remove sudo from create_debian_release.sh

--- ci/create_debian_release.sh
+++ ci/create_debian_release.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -xe
 
+OUTDIR=/base/debianbuild
+
 if [ $# -ne 1 ] && [ -z "${1}" ]; then
     echo "no version number specified"
     echo "${0} x.y.z"
@@ -9,17 +11,35 @@ fi
 NEW_VERSION="${1}"
 echo "Create release: ${NEW_VERSION}"
 
-export GIT_AUTHOR_NAME="Wolfgang Gehrhardt"
-export GIT_COMMITTER_NAME="${GIT_AUTHOR_NAME}"
-export DEBNAME="${GIT_AUTHOR_NAME}"
+GLOBMAIL=$(git config --global user.email || true)
+GLOBNAME=$(git config --global user.name || true)
 
-export EMAIL=${EMAIL-"wolfgang.gehrhardt@emlix.com"}
-export GIT_AUTHOR_EMAIL="${EMAIL}"
-export GIT_COMMITTER_EMAIL="${EMAIL}"
+LOCMAIL=$(git config --local user.email || true)
+LOCNAME=$(git config --local user.name || true)
+
+TMPNAME="${GIT_AUTHOR_NAME:-${LOCNAME:-${GLOBNAME:-}}}"
+
+if [ -z "${TMPNAME}" ]; then
+    echo "Invalid environment. Could not determine author name for git"
+    exit 1
+fi
+
+TMPMAIL="${GIT_AUTHOR_EMAIL:-${LOCMAIL:-${GLOBMAIL:-}}}"
+
+if [ -z "${TMPMAIL}" ]; then
+    echo "Invalid environment. Could not determine author e-mail for git"
+    exit 1
+fi
+
+export GIT_AUTHOR_EMAIL="${TMPMAIL}"
+export GIT_COMMITTER_EMAIL="${TMPMAIL}"
+export EMAIL="${TMPMAIL}"
+export GIT_AUTHOR_NAME="${TMPNAME}"
+export GIT_COMMITTER_NAME="${TMPNAME}"
+export NAME="${TMPNAME}"
+
+export DEBNAME="${NAME}"
 export DEBMAIL="${EMAIL}"
-
-git config --local user.name "${GIT_AUTHOR_NAME}"
-git config --local user.email "${GIT_AUTHOR_EMAIL}"
 
 gbp import-ref -u "${NEW_VERSION}" --debian-branch "$(git branch --show-current)"
 
@@ -34,4 +54,4 @@ dch -M "--newversion=${NEW_VERSION}-1" "New upstream tag ${NEW_VERSION}"
 git add debian/ && git commit -m "New upstream tag ${NEW_VERSION}"
 git checkout HEAD -- debian.native && git clean -fxd -- debian.native
 
-sudo gbp buildpackage --git-ignore-branch --git-compression=xz --git-ignore-new -uc -us
+gbp buildpackage --git-ignore-branch --git-compression=xz --git-ignore-new -uc -us --git-export-dir="${OUTDIR}" --git-ignore-branch
